e7cb8d598c1cb79a28df656f071930ca69639438
Pat Gavlin <pat@pulumi.com>
2018-07-06 11:28:17 -0700

    Document schemas and builtin providers.

--- il/builtin_providers.go
+++ il/builtin_providers.go
@@ -8,6 +8,10 @@ import (
 	"github.com/pulumi/pulumi-terraform/pkg/tfbridge"
 )
 
+// builtinProviderInfo provides a static map from provider name to propvider information for the small set of providers
+// that should be implemented by the target language (rather than as Pulumi providers). Currently this includes the
+// archive and http providers. Resources from the former provider are translated as Pulumi assets; resources/data
+// sources from the latter should be translated as calls to the target langauge's appropriate HTTP client libraries.
 var builtinProviderInfo = map[string]*tfbridge.ProviderInfo{
 	"archive": {
 		P:      archive.Provider().(*schema.Provider),
--- il/schemas.go
+++ il/schemas.go
@@ -5,12 +5,17 @@ import (
 	"github.com/pulumi/pulumi-terraform/pkg/tfbridge"
 )
 
+// Schemas bundles a property's Terraform and Pulumi schema information into a single type. This information is then
+// used to determine type and name information for the property. If the Terraform property is of a composite type--a
+// map, list, or set--the property's schemas may also be used to access child schemas.
 type Schemas struct {
 	TF     *schema.Schema
 	TFRes  *schema.Resource
 	Pulumi *tfbridge.SchemaInfo
 }
 
+// PropertySchemas returns the Schemas for the child property with the given name. This is only valid if the current
+// Schemas describe a map property.
 func (s Schemas) PropertySchemas(key string) Schemas {
 	var propSch Schemas
 
@@ -31,6 +36,7 @@ func (s Schemas) PropertySchemas(key string) Schemas {
 	return propSch
 }
 
+// ElemSchemas returns the element Schemas for a list property.
 func (s Schemas) ElemSchemas() Schemas {
 	var elemSch Schemas
 
@@ -50,6 +56,7 @@ func (s Schemas) ElemSchemas() Schemas {
 	return elemSch
 }
 
+// Type returns the appropriate bound type for the property associated with these Schemas.
 func (s Schemas) Type() Type {
 	if s.TF != nil {
 		switch s.TF.Type {
