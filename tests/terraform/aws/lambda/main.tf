163366cf4a7da7fffdb4d7f14aa3d267072458dd
Pat Gavlin <pat@pulumi.com>
2018-10-22 09:56:57 -0700

    Stop specifying resource names in AWS tests.
    Specifying explicit names for AWS resources harms the reliability of our
    CI jobs, as it can cause conflicts between jobs that run concurrently or
    jobs that fail to clean up their resources. By removing the explicit names,
    we can take advantage of Pulumi's auto-naming and avoid these
    situations.

--- tests/terraform/aws/lambda/main.tf
+++ tests/terraform/aws/lambda/main.tf
@@ -1,3 +1,8 @@
+# NOTE: we do not specify names for any of the test resources in order to improve the reliability of our CI jobs
+# in the face of parallelism and leftover resources. Explicitly naming these resources can cause conflicts
+# between jobs that run concurrently or jobs that fail to clean up their resources. Pulumi will auto-name these
+# for us.
+
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
@@ -26,12 +31,12 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+  # name               = "iam_for_lambda"
   assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name = "hello_lambda"
+  # function_name = "hello_lambda"
 
   filename         = "${data.archive_file.zip.output_path}"
   source_code_hash = "${data.archive_file.zip.output_sha}"
--- tests/terraform/aws/networking/subnet/security_group.tf
+++ tests/terraform/aws/networking/subnet/security_group.tf
@@ -1,5 +1,10 @@
+# NOTE: we do not specify names for any of the test resources in order to improve the reliability of our CI jobs
+# in the face of parallelism and leftover resources. Explicitly naming these resources can cause conflicts
+# between jobs that run concurrently or jobs that fail to clean up their resources. Pulumi will auto-name these
+# for us.
+
 resource "aws_security_group" "az" {
-  name        = "az-${data.aws_availability_zone.target.name}"
+  # name        = "az-${data.aws_availability_zone.target.name}"
   description = "Open access within the AZ ${data.aws_availability_zone.target.name}"
   vpc_id      = "${var.vpc_id}"
 
