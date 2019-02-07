0578c2dba2f27dead6f11faa3a8c32d16e9e78a2
Pat Gavlin <pat@pulumi.com>
2019-02-06 17:08:27 -0800

    Assign nicer names for apply args
    Rather than using `__argX`, derive the name for each apply argument from
    the name of its corresponding variable reference.

--- tests/terraform/aws/lb-listener/index.base.ts
+++ tests/terraform/aws/lb-listener/index.base.ts
@@ -7,19 +7,19 @@ const domain = new aws.cognito.UserPoolDomain("domain", {});
 const frontEndLoadBalancer = new aws.elasticloadbalancingv2.LoadBalancer("front_end", {});
 const frontEndTargetGroup = new aws.elasticloadbalancingv2.TargetGroup("front_end", {});
 const frontEndListener = new aws.elasticloadbalancingv2.Listener("front_end", {
-    defaultAction: pulumi.all([pool.arn, client.id, domain.domain, frontEndTargetGroup.arn]).apply(([__arg0, __arg1, __arg2, __arg3]) => (() => {
+    defaultAction: pulumi.all([pool.arn, client.id, domain.domain, frontEndTargetGroup.arn]).apply(([poolArn, id, domain, frontEndTargetGroupArn]) => (() => {
         throw "tf2pulumi error: aws_lb_listener.front_end.default_action: expected at most one item in list, got 2";
         return [
             {
                 authenticateCognito: {
-                    userPoolArn: __arg0,
-                    userPoolClientId: __arg1,
-                    userPoolDomain: __arg2,
+                    userPoolArn: poolArn,
+                    userPoolClientId: id,
+                    userPoolDomain: domain,
                 },
                 type: "authenticate-cognito",
             },
             {
-                targetGroupArn: __arg3,
+                targetGroupArn: frontEndTargetGroupArn,
                 type: "forward",
             },
         ];
