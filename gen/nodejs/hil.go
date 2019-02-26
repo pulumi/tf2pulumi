f9a0b276b963229844abbccb8163835285365293
Pat Gavlin <pat@pulumi.com>
2019-02-25 16:15:55 -0800

    Fix two binding issues
    - Support `terraform.workspace` references. The moral equivalent of the
      Terraform workspace name is the Pulumi stack name, so these turn into
      `pulumi.getStack()`.
    - Allow non-splat/indexed access to counted resources. This is allowed
      in Terraform when the count evaluates to 1.
    
    Fixes #68.

--- gen/nodejs/hil.go
+++ gen/nodejs/hil.go
@@ -212,6 +212,8 @@ func (g *generator) genCall(w io.Writer, n *il.BoundCall) {
 		g.genf(w, "new pulumi.asset.FileAsset(%v)", n.Args[0])
 	case "__coerce":
 		g.genCoercion(w, n.Args[0], n.Type())
+	case "__getStack":
+		g.genf(w, "pulumi.getStack()")
 	case "base64decode":
 		g.genf(w, "Buffer.from(%v, \"base64\").toString()", n.Args[0])
 	case "base64encode":
