72bfb03960f26408e1219f20fb053099b594ba0e
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-02-25 20:30:53 -0800

    Make it simpler to run go tests consistently (#72)

--- build/common.mk
+++ build/common.mk
@@ -153,6 +153,7 @@ endif
 ifneq ($(SUB_PROJECTS),)
 only_build:: $(SUB_PROJECTS:%=%_only_build)
 only_test:: $(SUB_PROJECTS:%=%_only_test)
+only_test_fast:: $(SUB_PROJECTS:%=%_only_test_fast)
 default:: $(SUB_PROJECTS:%=%_default)
 all:: $(SUB_PROJECTS:%=%_all)
 ensure:: $(SUB_PROJECTS:%=%_ensure)
@@ -214,6 +215,7 @@ endif
 
 only_build:: build install
 only_test:: lint test_all
+only_test_fast:: lint test_fast
 
 # Generate targets for each sub project. This project's default and
 # all targets will depend on the sub project's targets, and the
@@ -238,6 +240,8 @@ $(SUB_PROJECTS:%=%_only_build):
 	@$(MAKE) -C ./$(@:%_only_build=%) only_build
 $(SUB_PROJECTS:%=%_only_test):
 	@$(MAKE) -C ./$(@:%_only_test=%) only_test
+$(SUB_PROJECTS:%=%_only_test_fast):
+	@$(MAKE) -C ./$(@:%_only_test_fast=%) only_test_fast
 endif
 
 # As a convinece, we provide a format target that folks can build to
