b60adb7dc142ef361da038877953313d95b2708e
Sean Gillespie <sean@pulumi.com>
2019-02-26 17:42:42 -0800

    Python backend: emit simple resources
    This commit contains the necessary plumbing to correctly convert simple
    Terraform resources into Python. As a milestone, this commit also
    contains a regression test using the Random provider that correctly
    converts and runs.

--- gen/python/generator.go
+++ gen/python/generator.go
@@ -18,8 +18,16 @@
 package python
 
 import (
-	"errors"
+	"bytes"
+	"fmt"
 	"io"
+	"sort"
+	"strings"
+
+	"github.com/hashicorp/terraform/config"
+	"github.com/pkg/errors"
+	"github.com/pulumi/pulumi-terraform/pkg/tfbridge"
+	"github.com/pulumi/pulumi/pkg/util/contract"
 
 	"github.com/pulumi/tf2pulumi/gen"
 	"github.com/pulumi/tf2pulumi/il"
@@ -34,11 +42,49 @@ func New(projectName string, w io.Writer) gen.Generator {
 }
 
 type generator struct {
-	projectName string
-	w           io.Writer
+	projectName   string
+	w             io.Writer
+	needNYIHelper bool
+
+	// put here because of copy-pasta
+	// indent is the current indentation level for the generated source.
+	indent string
+	// countIndex is the name (if any) of the currently in-scope count variable.
+	countIndex string
+	// unknownInputs is the set of input variables that may be unknown at runtime.
+	unknownInputs map[*il.VariableNode]struct{}
 }
 
-func (g *generator) GeneratePreamble(gs []*il.Graph) error {
+func (g *generator) GeneratePreamble(modules []*il.Graph) error {
+	g.println("import pulumi")
+
+	// Accumulate other imports for the various providers. Don't emit them yet, as we need to sort them later on.
+	var imports []string
+	providers := make(map[string]bool)
+	for _, m := range modules {
+		for _, p := range m.Providers {
+			name := p.PluginName
+			if !providers[name] {
+				providers[name] = true
+				switch name {
+				case "archive":
+					return errors.New("NYI: Python Archive Provider")
+				case "http":
+					return errors.New("NYI: Python HTTP Provider")
+				default:
+					imports = append(imports, fmt.Sprintf("import pulumi_%[1]s as %[1]s", name))
+				}
+			}
+		}
+	}
+
+	// TODO(swgillespie) walk the graph to find optional imports
+
+	sort.Strings(imports)
+	for _, pkg := range imports {
+		g.println(pkg)
+	}
+	g.println("") // end the preamble with a newline, standard Python style.
 	return nil
 }
 
@@ -50,6 +96,7 @@ func (g *generator) BeginModule(mod *il.Graph) error {
 }
 
 func (g *generator) EndModule(mod *il.Graph) error {
+	g.genNYIHelper(g.w)
 	return nil
 }
 
@@ -69,7 +116,34 @@ func (g *generator) GenerateLocal(l *il.LocalNode) error {
 }
 
 func (g *generator) GenerateResource(r *il.ResourceNode) error {
-	return errors.New("NYI: Python Resources")
+	pkg, subpkg, class, err := resourceTypeName(r)
+	if err != nil {
+		return err
+	}
+	if subpkg != "" {
+		subpkg = "." + subpkg
+	}
+
+	// TODO(swgillespie) resource explicit dependencies
+	if len(r.ExplicitDeps) != 0 {
+		return errors.New("NYI: Python Explicit Dependencies")
+	}
+	if r.Config.Mode != config.ManagedResourceMode {
+		return errors.New("NYI: Python Data Source Invocation")
+	}
+	if r.Count != nil {
+		return errors.New("NYI: Python Resource Count")
+	}
+
+	qualifiedMemberName := fmt.Sprintf("%s%s.%s", pkg, subpkg, class)
+	inputs, _, err := g.computeProperty(r.Properties, false, "")
+	if err != nil {
+		return err
+	}
+
+	name := g.nodeName(r)
+	g.printf("%s%s = %s(%q%s)\n", g.indent, name, qualifiedMemberName, name, inputs)
+	return nil
 }
 
 func (g *generator) GenerateOutputs(os []*il.OutputNode) error {
@@ -77,4 +151,192 @@ func (g *generator) GenerateOutputs(os []*il.OutputNode) error {
 		return errors.New("NYI: Python Outputs")
 	}
 	return nil
+}
+
+// lowerToLiterals gives the generator a chance to lower certain elements into literals before code generation. It is
+// unclear whether or not this is useful for Python yet.
+func (g *generator) lowerToLiterals(prop il.BoundNode) (il.BoundNode, error) {
+	return prop, nil
+}
+
+// nodeName returns a name suitable for the given node. It consults the IL to determine a good name for the node,
+// returning the selected name.
+//
+// In the future, this name will need to be at least somewhat unique to avoid redefining local variables, since
+// Terraform namespaces names by resource and we do not. For now, this returns the Terraform name for a particular
+// resource, which may not be unique.
+func (g *generator) nodeName(n il.Node) string {
+	if res, ok := n.(*il.ResourceNode); ok {
+		return res.Config.Name
+	}
+
+	// Obviously not great...
+	return "unknown"
+}
+
+// cleanName takes a name visible in Terraform config and translates it to a form suitable for Python. This involves
+// working around keywords and other things that are otherwise not legal in Python identifiers. For now, this does
+// nothing and returns the name verbatim.
+func cleanName(name string) string {
+	return name
+}
+
+//
+// Copy-pasted stuff from the node backend. Don't modify anything below this line!
+//
+
+// print prints one or more values to the generator's output stream.
+func (g *generator) print(a ...interface{}) {
+	_, err := fmt.Fprint(g.w, a...)
+	contract.IgnoreError(err)
+}
+
+// println prints one or more values to the generator's output stream, followed by a newline.
+func (g *generator) println(a ...interface{}) {
+	g.print(a...)
+	g.print("\n")
+}
+
+// prinft prints a formatted message to the generator's output stream.
+func (g *generator) printf(format string, a ...interface{}) {
+	_, err := fmt.Fprintf(g.w, format, a...)
+	contract.IgnoreError(err)
+}
+
+// gen generates code for a list of strings and expression trees. The former are written directly to the destination;
+// the latter are recursively generated using the appropriate gen* functions.
+func (g *generator) gen(w io.Writer, vs ...interface{}) {
+	for _, v := range vs {
+		switch v := v.(type) {
+		case string:
+			_, err := fmt.Fprint(w, v)
+			contract.IgnoreError(err)
+		case *il.BoundArithmetic:
+			g.genArithmetic(w, v)
+		case *il.BoundCall:
+			g.genCall(w, v)
+		case *il.BoundConditional:
+			g.genConditional(w, v)
+		case *il.BoundIndex:
+			g.genIndex(w, v)
+		case *il.BoundLiteral:
+			g.genLiteral(w, v)
+		case *il.BoundOutput:
+			g.genOutput(w, v)
+		case *il.BoundPropertyExpr:
+			g.gen(w, v.Value)
+		case *il.BoundVariableAccess:
+			g.genVariableAccess(w, v)
+		case *il.BoundListProperty:
+			g.genListProperty(w, v)
+		case *il.BoundMapProperty:
+			g.genMapProperty(w, v)
+		case *il.BoundError:
+			g.genError(w, v)
+		default:
+			contract.Failf("unexpected type in gen: %T", v)
+		}
+	}
+}
+
+// genf generates code using a format string and its arguments. Any arguments that are BoundNode values are wrapped in
+// a FormatFunc that calls the appropriate recursive generation function. This allows for the composition of standard
+// format strings with expression/property code gen (e.g. `g.genf(w, ".apply(__arg0 => %v)", then)`, where `then` is
+// an expression tree).
+func (g *generator) genf(w io.Writer, format string, args ...interface{}) {
+	for i := range args {
+		if node, ok := args[i].(il.BoundNode); ok {
+			args[i] = gen.FormatFunc(func(f fmt.State, c rune) { g.gen(f, node) })
+		}
+	}
+	fmt.Fprintf(w, format, args...)
+}
+
+// computeProperty generates code for the given property into a string ala fmt.Sprintf. It returns both the generated
+// code and a bool value that indicates whether or not any output-typed values were nested in the property value.
+func (g *generator) computeProperty(prop il.BoundNode, indent bool, count string) (string, bool, error) {
+	// First:
+	// - retype any possibly-unknown module inputs as the appropriate output types
+	// - discover whether or not the property contains any output-typed expressions
+	containsOutputs := false
+	_, err := il.VisitBoundNode(prop, il.IdentityVisitor, func(n il.BoundNode) (il.BoundNode, error) {
+		if n, ok := n.(*il.BoundVariableAccess); ok {
+			if v, ok := n.ILNode.(*il.VariableNode); ok {
+				if _, ok = g.unknownInputs[v]; ok {
+					n.ExprType = n.ExprType.OutputOf()
+				}
+			}
+			containsOutputs = containsOutputs || n.Type().IsOutput()
+		}
+		return n, nil
+	})
+	contract.Assert(err == nil)
+
+	// Next, rewrite assets, lower certain constructrs to literals, insert any necessary coercions, and run the apply
+	// transform.
+	p, err := il.RewriteAssets(prop)
+	if err != nil {
+		return "", false, err
+	}
+
+	p, err = g.lowerToLiterals(p)
+	if err != nil {
+		return "", false, err
+	}
+
+	p, err = il.AddCoercions(p)
+	if err != nil {
+		return "", false, err
+	}
+
+	p, err = il.RewriteApplies(p)
+	if err != nil {
+		return "", false, err
+	}
+
+	// Finally, generate code for the property.
+	if indent {
+		g.indent += "    "
+		defer func() { g.indent = g.indent[:len(g.indent)-4] }()
+	}
+	g.countIndex = count
+	buf := &bytes.Buffer{}
+	g.gen(buf, p)
+	return buf.String(), containsOutputs, nil
+}
+
+// resourceTypeName computes the Python package, subpackage, and class name for a given resource.
+func resourceTypeName(r *il.ResourceNode) (string, string, string, error) {
+	// Compute the resource type from the Terraform type.
+	underscore := strings.IndexRune(r.Config.Type, '_')
+	if underscore == -1 {
+		return "", "", "", errors.New("NYI: single-resource providers")
+	}
+	provider, resourceType := cleanName(r.Provider.PluginName), r.Config.Type[underscore+1:]
+
+	// Convert the TF resource type into its Pulumi name.
+	memberName := tfbridge.TerraformToPulumiName(resourceType, nil, true)
+
+	// Compute the module in which the Pulumi type definition lives.
+	module := ""
+	if tok, ok := r.Tok(); ok {
+		components := strings.Split(tok, ":")
+		if len(components) != 3 {
+			return "", "", "", errors.Errorf("unexpected resource token format %s", tok)
+		}
+
+		mod, typ := components[1], components[2]
+
+		slash := strings.IndexRune(mod, '/')
+		if slash == -1 {
+			slash = len(mod)
+		}
+
+		module, memberName = mod[:slash], typ
+		if module == "index" {
+			module = ""
+		}
+	}
+
+	return provider, module, memberName, nil
 }
