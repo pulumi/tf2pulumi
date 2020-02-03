2e81dd7ebe3bb671eb6d895a0894cc0830460e8f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-02-03 12:18:38 -0800

    Fix test

--- tests/terraform/aws/cognito-user-pool/main.tf
+++ tests/terraform/aws/cognito-user-pool/main.tf
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "main" {
   # function_name = "terraform-example"
   role          = "${aws_iam_role.main.arn}"
   handler       = "exports.example"
-  runtime       = "nodejs8.10"
+  runtime       = "nodejs12.x"
 }
 
 resource "aws_iam_role" "cidp" {
