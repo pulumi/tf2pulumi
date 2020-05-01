b1d947d8a7f3d876d0fb8c094cfda329577647e7
Pat Gavlin <pat@pulumi.com>
2020-05-01 09:28:33 -0700

    Fix the fix

--- tests/terraform/aws/ec2/__main__.base.py
+++ tests/terraform/aws/ec2/__main__.base.py
@@ -1,7 +1,7 @@
 import pulumi
 import pulumi_aws as aws
 
-ubuntu = aws.get_ami(filter=[
+ubuntu = aws.get_ami(filters=[
         {
             "name": "name",
             "values": ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"],
